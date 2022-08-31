d9b79f9d261222547abfb6ef357cf4e2513a863d
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2022-08-31 21:19:58 +0200

    Save instance state, and LiveData object added

--- app/src/main/java/com/example/runningapplication/MainActivity.java
+++ app/src/main/java/com/example/runningapplication/MainActivity.java
@@ -1,6 +1,10 @@
 package com.example.runningapplication;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
 
 import android.os.Bundle;
 
@@ -10,10 +14,37 @@ public class MainActivity extends AppCompatActivity {
 
     private ActivityMainBinding binding;
 
+    private MyViewModel myViewModel;
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        if(myViewModel.getCaloriesBurned().getValue() != null){
+            outState.putInt(MyViewModel.CALORIES_BURNED_KEY, myViewModel.getCaloriesBurned().getValue());
+        }
+
+    }
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+        myViewModel = new ViewModelProvider(this).get(MyViewModel.class);
+        myViewModel.initByInstanceStateBundle(savedInstanceState);
+
+        myViewModel.getCaloriesBurned().observe(this,
+                caloriesBurned -> binding.textView.setText(caloriesBurned + "kcal")
+        );
+
+        binding.button.setOnClickListener(view -> {
+            double duration = Double.parseDouble(binding.editText.getText().toString());
+            double met = 9.8;
+            double weight = 80;
+            int caloriesBurned = (int) (duration * met * 3.5 * weight / 200);
+            myViewModel.setCaloriesBurned(caloriesBurned);
+        });
     }
 }
