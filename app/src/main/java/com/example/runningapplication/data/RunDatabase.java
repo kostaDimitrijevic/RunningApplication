9a9745fdd52a13e29e33f1acd918226a86ed7e5a
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2022-09-08 18:19:49 +0200

    Add DateConverter and make entity fields accessible

--- app/src/main/java/com/example/runningapplication/data/RunDatabase.java
+++ app/src/main/java/com/example/runningapplication/data/RunDatabase.java
@@ -2,8 +2,11 @@ package com.example.runningapplication.data;
 
 import androidx.room.Database;
 import androidx.room.RoomDatabase;
+import androidx.room.TypeConverters;
+
 
 // koji entiteti idu u bazu podataka. Versija je bitna
+@TypeConverters(value = {DateConverter.class})
 @Database(entities = {Workout.class}, version = 1, exportSchema = false)
 public abstract class RunDatabase extends RoomDatabase {
 
--- app/src/main/java/com/example/runningapplication/data/Workout.java
+++ app/src/main/java/com/example/runningapplication/data/Workout.java
@@ -17,4 +17,51 @@ public class Workout {
     private double distance;
     private double duration;
 
+    public Workout(long id, Date date, String label, double distance, double duration) {
+        this.id = id;
+        this.date = date;
+        this.label = label;
+        this.distance = distance;
+        this.duration = duration;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public String getLabel() {
+        return label;
+    }
+
+    public double getDistance() {
+        return distance;
+    }
+
+    public double getDuration() {
+        return duration;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
+
+    public void setLabel(String label) {
+        this.label = label;
+    }
+
+    public void setDistance(double distance) {
+        this.distance = distance;
+    }
+
+    public void setDuration(double duration) {
+        this.duration = duration;
+    }
 }
