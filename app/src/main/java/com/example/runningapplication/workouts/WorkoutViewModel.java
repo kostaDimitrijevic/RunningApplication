70c2b612728bb8b154ab033701a85975355417ca
Kosta Dimitrijevic <kole.dimi99@gmail.com>
2022-09-10 16:38:14 +0200

    SavedStateHandle added for view model

--- app/src/main/java/com/example/runningapplication/workouts/WorkoutViewModel.java
+++ app/src/main/java/com/example/runningapplication/workouts/WorkoutViewModel.java
@@ -1,7 +1,7 @@
 package com.example.runningapplication.workouts;
 
 import androidx.lifecycle.LiveData;
-import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.SavedStateHandle;
 import androidx.lifecycle.Transformations;
 import androidx.lifecycle.ViewModel;
 
@@ -12,18 +12,36 @@ import java.util.List;
 
 import javax.inject.Inject;
 
+import dagger.assisted.Assisted;
 import dagger.hilt.android.lifecycle.HiltViewModel;
 
 @HiltViewModel
 public class WorkoutViewModel extends ViewModel {
 
     private final WorkoutRepository workoutRepository;
+    private final SavedStateHandle savedStateHandle;
 
-    private final MutableLiveData<Boolean> sorted = new MutableLiveData<>(false);
+    private static final String SORTED_KEY = "sorted-key";
+
+    private final LiveData<List<Workout>> workouts;
+    private boolean sorted = false;
 
     @Inject
-    public WorkoutViewModel(WorkoutRepository workoutRepository) {
+    public WorkoutViewModel(SavedStateHandle savedStateHandle, WorkoutRepository workoutRepository) {
         this.workoutRepository = workoutRepository;
+        this.savedStateHandle = savedStateHandle;
+
+        workouts = Transformations.switchMap(
+                savedStateHandle.getLiveData(SORTED_KEY, false),
+                sorted ->{
+                    if(!sorted){
+                        return workoutRepository.getAllLiveData();
+                    }
+                    else{
+                        return workoutRepository.getAllSortedLiveData();
+                    }
+                }
+        );
     }
 
     public void insertWorkout(Workout workout){
@@ -32,17 +50,10 @@ public class WorkoutViewModel extends ViewModel {
 
     public LiveData<List<Workout>> getWorkoutList(){
 
-        return Transformations.switchMap(sorted, sorted -> {
-            if(!sorted){
-                return workoutRepository.getAllLiveData();
-            }
-            else{
-                return workoutRepository.getAllSortedLiveData();
-            }
-        });
+        return workouts;
     }
 
     public void invertSorted(){
-        sorted.setValue(!sorted.getValue());
+        savedStateHandle.set(SORTED_KEY, sorted = !sorted);
     }
 }
